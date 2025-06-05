c8bb79b960d76a0ef6961aa79736ec822c60c9d9
yilinpotato <qqrtqr66@163.com>
2025-06-05 14:13:53 +0800

    开始面板结束面板

---
+++ unity gaocheng/Assets/MapAsset/scripts/ClickBotton.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ClickBotton : MonoBehaviour
+{
+   public void StartBottonClick()
+    {
+        SceneManager.LoadScene("MapScene");
+    }
+   public void EndBottonClick()
+    {
+        SceneManager.LoadScene("Ω·À„√Ê∞Â");
+    } 
+}
